d1a4c60c9fc91338401989b0fa2d771db609ca4c
Dylan Wreggelsworth <dylan@bvr.io>
2016-11-11 12:56:33 -0500

    Initial Commit

---
+++ GistSwift/GistSwift.h
@@ -0,0 +1,19 @@
+//
+//  GistSwift.h
+//  GistSwift
+//
+//  Created by Dylan Wreggelsworth on 11/11/16.
+//  Copyright © 2016 Dylan Wreggelsworth. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GistSwift.
+FOUNDATION_EXPORT double GistSwiftVersionNumber;
+
+//! Project version string for GistSwift.
+FOUNDATION_EXPORT const unsigned char GistSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GistSwift/PublicHeader.h>
+
+
